29365f2cba1e3dd42fbd4b03f1c8eac1cb3b4808
Aziz0307 <Aziz03072018@gmail.com>
2024-12-04 17:56:18 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n= Convert.ToInt32(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+         double i=1;
+         while (n >= 1)
+         {
+             i *= n;
+             n -= 2;
+         }
+         Console.WriteLine($"Dvaynoy factoriak raven {i}");
+        }
+    }
+}
